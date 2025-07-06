dbd696c3f5cd9f123b8ee4132ad56933f0274360
royke <keane032@gmail.com>
2025-07-06 20:03:38 -0300

    remove text

--- cap10/k-vizinos.py
+++
@@ -1,7 +0,0 @@
-# K-vizinhos mais próximos (KNN) é um tipo de algoritmo de aprendizado supervisionado usado tanto para regressão quanto para classificação. 
-# O KNN tenta prever a classe correta para os dados de teste calculando a distância entre os dados de teste e todos os pontos de treinamento. 
-# Em seguida, seleciona-se o número K de pontos mais próximos dos dados de teste. O algoritmo KNN calcula a probabilidade de os dados de teste 
-# pertencerem às classes de dados de treinamento 'K' e a classe com maior probabilidade de ser selecionada. 
-# No caso de regressão, o valor é a média dos 'K' pontos de treinamento selecionados.
-# vizinhos mais próximos (KNN) é um tipo de algoritmo de aprendizagem supervisionada usado para regressão e classeificação. 
-
